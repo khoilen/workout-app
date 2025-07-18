17935352ce69fc4e0fc719fc710d2832c08e4419
khoinashtech <khoilen@nashtechglobal.com>
2025-07-18 15:32:19 +0700

    feat: add handle add habits

--- app/(tabs)/add-habit.tsx
+++ app/(tabs)/add-habit.tsx
@@ -1,22 +1,108 @@
-import { View } from "react-native";
-import { Button, SegmentedButtons, TextInput } from "react-native-paper";
+import { DATABASE_ID, databases, HABITS_COLLECTION_ID } from "@/libs/appwrite";
+import { useAuth } from "@/libs/auth-content";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { ID } from "react-native-appwrite";
+import {
+  Button,
+  SegmentedButtons,
+  Text,
+  TextInput,
+  useTheme,
+} from "react-native-paper";
 
 const FREQUENCY_OPTIONS = ["daily", "weekly", "monthly"];
 
+type Frequency = (typeof FREQUENCY_OPTIONS)[number];
+
 export default function AddHabit() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [frequency, setFrequency] = useState<Frequency>("daily");
+  const [error, setError] = useState<string | null>(null);
+
+  const { user } = useAuth();
+  const router = useRouter();
+  const theme = useTheme();
+
+  const handleSubmit = async () => {
+    try {
+      if (!user) return;
+
+      await databases.createDocument(
+        DATABASE_ID,
+        HABITS_COLLECTION_ID,
+        ID.unique(),
+        {
+          user_id: user.$id,
+          title,
+          description,
+          frequency,
+          streak_count: 0,
+          last_completed: new Date().toISOString(),
+          created_at: new Date().toISOString(),
+        }
+      );
+
+      router.back();
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+        return;
+      }
+
+      setError("An unexpected error occurred while adding the habit.");
+    }
+  };
+
   return (
-    <View>
-      <TextInput label="Title" mode="outlined" />
-      <TextInput label="Description" mode="outlined" />
-      <View>
+    <View style={styles.container}>
+      <TextInput
+        label="Title"
+        style={styles.input}
+        onChangeText={setTitle}
+        mode="outlined"
+      />
+      <TextInput
+        label="Description"
+        style={styles.input}
+        onChangeText={setDescription}
+        mode="outlined"
+      />
+      <View style={styles.frequencyContainer}>
         <SegmentedButtons
+          value={frequency}
+          onValueChange={(value) => setFrequency(value as Frequency)}
           buttons={FREQUENCY_OPTIONS.map((freq) => ({
             value: freq,
             label: freq.charAt(0).toUpperCase() + freq.slice(1),
           }))}
         />
       </View>
-      <Button mode="contained">Add Habit</Button>
+      <Button
+        mode="contained"
+        onPress={handleSubmit}
+        disabled={!title || !description}
+      >
+        Add Habit
+      </Button>
+      {error && <Text style={{ color: theme.colors.error }}>{error}</Text>}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#f5f5f5",
+    justifyContent: "center",
+  },
+  input: {
+    marginBottom: 16,
+  },
+  frequencyContainer: {
+    marginBottom: 24,
+  },
+});
