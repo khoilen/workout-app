819ea2b3486480d754205be712b6eacc6962c33a
khoinashtech <khoilen@nashtechglobal.com>
2025-07-25 05:39:52 +0700

    feat: add fetch habbits

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,150 @@
+import { DATABASE_ID, databases, HABITS_COLLECTION_ID } from "@/libs/appwrite";
 import { useAuth } from "@/libs/auth-content";
-import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import { Habit } from "@/types/database.type";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Query } from "react-native-appwrite";
+import { Button, Surface, Text } from "react-native-paper";
 
 export default function Index() {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const [habits, setHabits] = useState<Habit[]>([]);
+
+  useEffect(() => {
+    fetchHabits();
+  }, [user]);
+
+  const fetchHabits = async () => {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        HABITS_COLLECTION_ID,
+        [Query.equal("user_id", user?.$id ?? "")]
+      );
+
+      setHabits(response.documents as Habit[]);
+    } catch (error) {
+      console.error("Error fetching habits:", error);
+    }
+  };
+
   return (
-    <View style={styles.view}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Button mode="text" onPress={signOut} icon="logout">
-        Sign out
-      </Button>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text variant="headlineSmall" style={styles.title}>
+          Today habits
+        </Text>
+        <Button mode="text" onPress={signOut} icon="logout">
+          Signout
+        </Button>
+      </View>
+      {habits.length === 0 ? (
+        <View style={styles.emptyState}>
+          <Text style={styles.emptyText}>No habits found</Text>
+        </View>
+      ) : (
+        <View>
+          {habits.map((habit) => (
+            <Surface style={styles.card} key={habit.$id} elevation={0}>
+              <View key={habit.$id} style={styles.cardContent}>
+                <Text style={styles.cardTitle}>{habit.title}</Text>
+                <Text style={styles.cardDescription}>{habit.description}</Text>
+                <View style={styles.cardFooter}>
+                  <View style={styles.streakBadge}>
+                    <MaterialCommunityIcons
+                      name="fire"
+                      size={18}
+                      color={"#ff9800"}
+                    />
+                    <Text style={styles.streakText}>
+                      {habit.streak_count} day streak
+                    </Text>
+                  </View>
+                  <View style={styles.frequencyBadge}>
+                    <Text style={styles.frequencyText}>
+                      {habit.frequency.charAt(0).toUpperCase() +
+                        habit.frequency.slice(1)}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            </Surface>
+          ))}
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  view: {
+  container: {
     flex: 1,
-    justifyContent: "center",
+    padding: 16,
+    backgroundColor: "#f5f5f5",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
+    marginBottom: 16,
+  },
+  title: {
+    fontWeight: "bold",
+  },
+  card: {
+    marginBottom: 18,
+    padding: 18,
+    backgroundColor: "#f7f7fa",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.08,
+    shadowRadius: 8,
+    elevation: 4,
+    borderRadius: 8,
+  },
+  cardContent: {
+    padding: 3,
+  },
+  cardTitle: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginBottom: 16,
+    color: "#6c6c80",
+  },
+  cardDescription: {
+    fontSize: 15,
+    color: "#6c6c80",
+    marginBottom: 16,
+  },
+  cardFooter: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  streakBadge: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff3e0",
+    borderRadius: 12,
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+  },
+  streakText: {
+    fontSize: 14,
+    color: "#ff9800",
+    fontWeight: "bold",
+  },
+  frequencyBadge: {
+    backgroundColor: "#ede7f6",
+    borderRadius: 12,
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+  },
+  frequencyText: {
+    fontSize: 14,
+    color: "#7c4dff",
+    fontWeight: "bold",
   },
   navButton: {
     width: 100,
@@ -26,5 +152,14 @@ const styles = StyleSheet.create({
     backgroundColor: "coral",
     borderRadius: 8,
     textAlign: "center",
+  },
+  emptyState: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  emptyText: {
+    fontSize: 18,
+    color: "#6c6c80",
   },
 });
---
+++ types/database.type.ts
@@ -0,0 +1,11 @@
+import { type Models } from "react-native-appwrite";
+
+export interface Habit extends Models.Document {
+  user_id: string;
+  title: string;
+  description: string;
+  frequency: string;
+  streak_count: number;
+  created_at: Date;
+  updated_at: Date;
+}
